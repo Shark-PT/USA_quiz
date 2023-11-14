e04f2d9befd765395d4f227f7a6ac21a235d5273
Shark-PT <jccreis@hotmail.com>
2023-11-14 13:40:13 +0000

    pontuação e loop do jogo criado

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
-
+score = 0
 states = pandas.read_csv("50_states.csv")
+game_is_on = True
 
-answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?").capitalize()
-print(answer_state)
+while game_is_on:
+    answer_state = screen.textinput(title=f"{score}/50 Guess The State", prompt="What's another state's name?").capitalize()
+    print(answer_state)
 
-if answer_state in states.state.values:
-    screen.write(align = "state.x.values")
+    if answer_state in states.state.values:
+        score += 1
+        #screen.write(states.state.values, align = "state.x.values")
+    else:
+        game_is_on = False
