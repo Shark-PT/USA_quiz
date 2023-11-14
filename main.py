3802f7931bdc2d83c593912e61d85bcf27582b1b
Shark-PT <jccreis@hotmail.com>
2023-11-14 13:09:39 +0000

    Configuração ecrã inicial

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+
+answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?")
+
