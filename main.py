7c6781eb109f17478b0ed8cc348d1bb257536076
Shark-PT <jccreis@hotmail.com>
2023-11-14 13:33:15 +0000

    inicio do jogo. Input do user pronto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,10 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
+states = pandas.read_csv("50_states.csv")
 
-answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?")
+answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?").capitalize()
+print(answer_state)
 
+if answer_state in states.state.values:
+    screen.write(align = "state.x.values")
