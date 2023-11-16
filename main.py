0aec2ebcce818aad8c2585ac95e1365031034e80
Shark-PT <jccreis@hotmail.com>
2023-11-16 13:13:12 +0000

    Correção de Bugs, e implementação da funcionalidade states_to_learn

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ answers = []
 while len(answers) < 50:
     answer_state = (screen.textinput(title=f"{score}/50 Guess The State", prompt="What's another state's name?")
                     .title())
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in answers:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x), int(state_data.y))
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
         t.write(answer_state)
         score += 1
         answers.append(answer_state)
